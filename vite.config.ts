9791741cb29081c358dbb8a64cb41f0f57994dbc
Ayush Satyavarpu <ayushsatyavarpu@gmail.com>
2025-05-03 17:30:01 -0400

    prod depl issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/', // This should match your deployment base path
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
