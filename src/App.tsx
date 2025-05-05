e8af1343187c50b988ce219631b0b24badbc417a
Ayush Satyavarpu <ayushsatyavarpu@gmail.com>
2025-05-05 16:47:46 -0400

    post rebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Callback from './callback';
-import LandingPage from './LandingPage.tsx';
 import CheckoutForm from './CheckoutForm.tsx';
 import Return from './CheckoutReturn.tsx';
 import LandingPage from './LandingPage.tsx';
-import tailwind from './tailwind.css';
 
 
 function App() {
