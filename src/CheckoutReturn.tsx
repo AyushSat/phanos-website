3be43c487e9806b0d83cb2acbd4b0a0b86e9fc7d
Ayush Satyavarpu <ayushsatyavarpu@gmail.com>
2025-05-05 16:57:02 -0400

    changed email

--- src/CheckoutReturn.tsx
+++ src/CheckoutReturn.tsx
@@ -28,7 +28,7 @@ const Return = () => {
             <p>
             We appreciate your business! A confirmation email will be sent to {customerEmail}.
 
-            If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
+            If you have any questions, please email <a href="mailto:phanos.app@gmail.com">phanos.app@gmail.com</a>.
             </p>
         </section>
         )
